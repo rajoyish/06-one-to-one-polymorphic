33110f88ab59c366b619c02fd1c5c50dd7a0aa85
Himalayan <hdnpress@gmail.com>
2023-02-08 14:29:14 +0545

    Post & User share polymorphic relation to Image model. The unique Image(s) are associated with Post(s) and user(s)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
